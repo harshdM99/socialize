ce9088c8a86e15c577ad598b7136357268c7cfa0
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-06-11 22:53:04 +0530

    add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function(req, res){
+    res.end("<h1> Express js is running for codeial! </h1>");
+}
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// app.use("/", require("./routes/index"));
+// Default is index so we can only give the directory
+app.use("/", require("./routes"));
+
 app.listen(port, function(err){
     if(err){
         console.error(`Error in running the server : ${err}`);
