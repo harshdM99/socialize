75f7e82ffee192231494f3d39a4b661a78332533
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-06-12 19:52:21 +0530

    setup the view engine

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = 8000;
 // Default is index so we can only give the directory
 app.use("/", require("./routes"));
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, function(err){
     if(err){
         console.error(`Error in running the server : ${err}`);
