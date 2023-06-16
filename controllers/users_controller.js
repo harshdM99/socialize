5304bca118ba0ade3b3c2d394614a5c710fb5a95
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-06-16 22:43:25 +0530

    rename ejs file from user to user_profile

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 
 module.exports.profile = function(req, res){
     // res.end("<h1> I am a user profile! </h1>");
-    return res.render("user", {
+    return res.render("user_profile", {
         title: "User",
     });
 }
