dd204565b6a500c150d9988e5eb615b62c57918e
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-06-18 00:40:51 +0530

    add sign out page and complete the functionality

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -57,3 +57,12 @@ module.exports.create = function(req, res){
 module.exports.createSession = function(req, res) {
     return res.redirect("/users/profile");
 }
+
+// sign out from the session
+module.exports.destroySession = function(req, res) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+    
+        res.redirect('/');
+    });
+}
