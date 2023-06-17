c6b0f189f839f4b50cca06871ce13d74c473348d
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-06-17 22:43:54 +0530

    fix passport.js setup error

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -8,20 +8,19 @@ passport.use(new localStrategy({
     },
     function(email, password, done){
         // find a user and establish the identity
-        User.findOne({email: email}, function(err, user){
-            if(err){
-                console.log("Error in finding user -> passport");
-                return done(err);
-            }
-
+        User.findOne({email: email}).then((user)=>{
             if(!user || user.password != password){
                 console.log("Invalid Username/Password");
                 return done(null, false);
             }
             return done(null, user);
+        }).catch((err)=>{
+            if(err){
+                console.log("Error in finding user -> passport");
+                return done(err);
+            }
         });
-    }
-));
+}));
 
 // serializing to decide which key is to be kept in the cookies
 passport.serializeUser(function(user, done){
@@ -30,13 +29,11 @@ passport.serializeUser(function(user, done){
 
 // deserializing the user from the key in the cookies
 passport.deserializeUser(function(id, done){
-    User.findById(id, function(err, user){
-        if(err){
-            console.log("Error in finding user -> passport");
-            return done(err);
-        }
-
+    User.findById(id).then((user)=>{
         return done(null, user);
+    }).catch((err)=>{
+        console.log("Error in finding user -> passport");
+        return done(err);
     });
 });
 
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 8080;
 const expressLayouts = require("express-ejs-layouts");
 const db = require("./config/mongoose");
+// used for session cookie
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
 
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
@@ -15,10 +19,25 @@ app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 // app.use("/", require("./routes/index"));
 // Default is index so we can only give the directory
-app.use("/", require("./routes"));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+app.use(session({
+    name: "socialize",
+    // TODO : change the secret before deployment in production
+    secret: "randomText",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", require("./routes"));
 
 app.listen(port, function(err){
     if(err){
