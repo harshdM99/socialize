8af5ae9a541f5d40d3c267996459fb9105e0b32e
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-06-25 21:35:43 +0530

    fix home page authentication

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,12 @@
 module.exports.home = function(req, res){
-    // res.end("<h1> Express js is running for codeial! </h1>");
-    
     // console.log(req.cookies);
     // update a cookie
     // res.cookie("user_id", 25);
-    return res.render("home", {
-        title: "Home",
-    });
+    if(req.isAuthenticated()){
+        return res.render("home", {
+            title: "Home",
+        });
+    }
+    
+    return res.redirect("/users/sign-in/");    
 }
