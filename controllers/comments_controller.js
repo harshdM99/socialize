235979234e26b39fed2a59de42dcf1573d684adc
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-06-28 18:57:14 +0530

    add functionality to delete comments

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -26,3 +26,27 @@ module.exports.create = async function(req, res) {
         return res.redirect("/");
     }
 }
+
+module.exports.destroy = async function(req, res) {
+    try {
+        const commentToDelete = await Comment.findById(req.params.id);
+
+        // TODO : I can delete comments from other users on my post as well 
+        // get the user who made the post
+        let userOfPost = await Post.findById(commentToDelete.post);
+        userOfPost = userOfPost.user
+        
+        if(commentToDelete.user == req.user.id || userOfPost == req.user.id ) {
+            let postId = commentToDelete.post;
+            
+            await Comment.findOneAndDelete({_id: req.params.id});
+            await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+        }
+
+        return res.redirect("back");
+    }
+    catch(err) {
+        console.error(err);
+        return res.redirect("back");
+    }
+}
