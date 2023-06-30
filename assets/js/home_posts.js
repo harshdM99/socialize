f4844c540b76d4ae747762fa3c2576b143d039c9
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-07-01 00:38:46 +0530

    add method to create a post in DOM

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,4 +1,5 @@
 // method to submit form data for new post using ajax
+
 let createPost = function () {
     let newPostForm = $("#new-post-form");
     newPostForm.on("submit", function(event) {
@@ -9,16 +10,42 @@ let createPost = function () {
             url: "/posts/create",
             data: newPostForm.serialize(),
             success: function(data) {
-                console.log(data);
+                let newPost = newPostDom(data.data.post);
+                $("#post-list-container>ul").prepend(newPost);
             }, 
             error: function(err) {
                 console.log(err.responseText);
             }
         });
+        newPostForm.trigger("reset");
     });
 }
 
 // method to create a post in DOM
+let newPostDom = function(post) {
+    return $(`<li id="post-${post._id}">
+                <p>
+                    <small>
+                        <a class="delete-post-button" href="/posts/delete/${post.id}"> X </a>
+                    </small>
+                    <strong> ${post.content} </strong> <br>
+                    <small> ${post.user.name} </small>
+                </p>
 
+                <div class="post-comments">
+                    <form action="/comments/create" method="POST">
+                        <input type="text" name="content" placeholder="Type here for comment..">
+                        <input type="hidden" name="post_id" value="${ post._id }">
+                        <input type="submit" value="Add Comment">
+                    </form>
+
+                    <div class="post-comments-list">
+                        <ul id="post-comments-${ post._id }">
+                            
+                        </ul>
+                    </div>
+                </div>
+            </li>`);
+}
 
 createPost();
